e8ff892869e082770ee3a08bd17b0e3dd57aad2b
RexShack <rexshackuk@gmail.com>
2025-12-11 21:15:08 +0000

    update

--- jobs.lua
+++ jobs.lua
@@ -151,5 +151,89 @@ RSGShared.Jobs = {
             ['2'] = { name = 'Master Trainer', isboss = true, payment = 15 },
         },
     },
+     govenor1 = {
+        label = 'Govenor New Hanover',
+        type = 'govenor',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Govenor',
+                payment = 100
+            },
+        },
+    },
+    govenor2 = {
+        label = 'Govenor West Elizabeth',
+        type = 'govenor',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Govenor',
+                payment = 100
+            },
+        },
+    },
+    govenor3 = {
+        label = 'Govenor New Austin',
+        type = 'govenor',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Govenor',
+                payment = 100
+            },
+        },
+    },
+    govenor4 = {
+        label = 'Govenor Ambarino',
+        type = 'govenor',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Govenor',
+                payment = 100
+            },
+        },
+    },
+    govenor5 = {
+        label = 'Govenor Lemoyne',
+        type = 'govenor',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Govenor',
+                payment = 100
+            },
+        },
+    },
+    annesburgminer = {
+        name = 'annesburgminer',
+        label = 'Annesburg Miner',
+        type = 'miner',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = { name = 'Recruit', payment = 3 },
+            ['1'] = { name = 'Miner',   payment = 5 },
+            ['2'] = { name = 'Forman', isboss = true, payment = 15 },
+        },
+    },
+    gaptoothminer = {
+        name = 'gaptoothminer',
+        label = 'Gaptooth Miner',
+        type = 'miner',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = { name = 'Recruit', payment = 3 },
+            ['1'] = { name = 'Miner',   payment = 5 },
+            ['2'] = { name = 'Forman', isboss = true, payment = 15 },
+        },
+    },
 
 }
